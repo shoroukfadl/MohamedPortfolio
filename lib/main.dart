8dc3c36a2a05bca9c75c886bd4570475e696e19f
shorouk <shoroukfadl975@gmail.com>
2026-06-11 21:03:32 +0300

    format

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,10 @@ void main() async {
   );
 
 
-  debugPrint("YRL : ${SupabaseConfig.url}");
   debugPrint("ANON : ${SupabaseConfig.anonKey}");
-  await Supabase.initialize(url:'https://zljhjxsdlidmfpfwmvpa.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsamhqeHNkbGlkbWZwZndtdnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Njk3NzQsImV4cCI6MjA5MjM0NTc3NH0.Vv0wRPesQi7BG-iw5SECV_yUmHeZ8IYqlGJreN0thu4');
+  await Supabase.initialize(url:SupabaseConfig.url, anonKey:SupabaseConfig.anonKey );
   await GitIt.initGitIt();
-  setHashUrlStrategy();
+  setPathUrlStrategy();
   runApp(MultiBlocProvider(providers: [
     BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
     BlocProvider<AppLanguage>(create: (_) => AppLanguage()),
